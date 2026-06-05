7beccee3f0b125b81c17b5cd35439472eea85696
saniyababar424-cloud <saniyababar424@gmail.com>
2026-06-05 08:52:49 +0530

    Three statements

---
+++ threeStatements.java
@@ -0,0 +1,7 @@
+public class threeStatements {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("I am Saniya");
+        System.out.println("How are you");
+    }
+}
